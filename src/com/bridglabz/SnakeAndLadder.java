79a01183ade66f47248f017cd6847363a857d2a6
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-23 12:48:09 +0530

    Added;Get die values between 0 to 6 programme

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -5,6 +5,7 @@ public class SnakeAndLadder {
     static public final int Ending_Position_Of_Player=100;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder ");
-
+int die_Roll=(int)Math.floor((Math.random()*10)%7);
+        System.out.println(die_Roll);
     }
 }

28d3f7ec4aca007211e2d3ae6bd03ab72c7b47f2
akshayzagad <akshayzagade62@gmail.com.com>
2022-11-23 12:36:21 +0530

    Added;Start Position in programme

--- src/com/bridglabz/SnakeAndLadder.java
+++ src/com/bridglabz/SnakeAndLadder.java
@@ -1,7 +1,10 @@
 package com.bridglabz;
 
 public class SnakeAndLadder {
+    static public final int Starting_Position_Of_Player=0;
+    static public final int Ending_Position_Of_Player=100;
     public static void main(String[] args) {
         System.out.println("Welcome to Snake And Ladder ");
+
     }
 }
